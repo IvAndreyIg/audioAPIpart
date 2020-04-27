899c2fe0429542bb44bf477e9a88f7d55438a65a
IvAndreyIg <frostpunchofpain@gmail.com>
2020-04-27 13:37:38 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const buzova='jagodica.mp3';
+let b=document.querySelector("#Bib");
+
+console.log("Bstart1");
+b.onclick=()=>{
+  console.log("Bstart");
+  let cont=new AudioContext();
+  let arr=cont.createBufferSource();
+
+
+  window.fetch(buzova)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => cont.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+
+      arr.buffer = audioBuffer;
+      console.log('audiobuffer');
+      //console.log(audioBuffer);
+    });
+  setTimeout(()=>{
+    arr.connect(cont.destination);
+    arr.playbackRate.value=1.5;
+    arr.start();
+
+
+  },600);
+
+
+
+};
+
+
+
+
+
