a51fce60df808539adc4d515efb67e647ffacda2
bang <frostpunchofpain@gmail.com>
2020-04-28 19:19:21 +0300

    up3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const buzova='js/Down.mp3';
+const buzova='js/jagodica.mp3';
 let b=document.querySelector("#Bib");
 
 
@@ -13,52 +13,59 @@ function start(cont,buffer) {
 
 
     arr.buffer=buffer;
-    let LData = arr.buffer.getChannelData(1);
-  let RData = arr.buffer.getChannelData(1);
-  console.log(arr.buffer.duration);
-    ctx.strokeStyle="red";
-    ctx.lineWidth=3;
+    let DataChen=[arr.buffer.getChannelData(0),arr.buffer.getChannelData(1)];
+  //  let LData = arr.buffer.getChannelData(1);
+//  console.log(arr.buffer.duration);
+  let Colours=["rgba(0,4,230,0.5)","rgba(250,0,0,0.5)"]
+    //ctx.strokeStyle="rgba(0,4,230,0.5)";
+    //ctx.strokeStyle="rgba(250,0,0,0.5)";
+    ctx.lineWidth=9;
   console.log(`che:${arr.buffer.numberOfChannels}`)
    // ctx.lineCap="round";
   let lenthCoff=32;
-let blocksSize=Math.floor(LData.length/Math.floor(arr.buffer.duration*lenthCoff));
+let blocksSize=Math.floor(DataChen[0].length/Math.floor(arr.buffer.duration*lenthCoff));
   console.log(`Blocks:${blocksSize}`);
   //420-min
 //7-sec
+
 let BlockPos=0;
   for(let i=0;i<Math.floor(arr.buffer.duration)*lenthCoff;i+=1){
     BlockPos+=blocksSize
-    ctx.beginPath();
+
    // if(i>300){
       //ctx.strokeStyle="blue";
    // }
+    for(let j=0;j<arr.buffer.numberOfChannels;j++){
+      ctx.strokeStyle=Colours[j];
+      ctx.beginPath();
     ctx.moveTo(i+10,300);
     ctx.lineTo(i+10,300-
 
-      //LData[(BlockPos)]
-      (LData[(BlockPos)]
-      +LData[BlockPos+1]
-      +LData[BlockPos+2]
-      +LData[BlockPos+3]
-      +LData[BlockPos+4])/5
+
+      (DataChen[j][(BlockPos)]
+      +DataChen[j][BlockPos+1]
+      +DataChen[j][BlockPos+2]
+      +DataChen[j][BlockPos+3]
+      +DataChen[j][BlockPos+4])/5
 
 
 
 
       *100);
     ctx.stroke();
+    }
   }
   console.log(`BlockPos${BlockPos}`);
 
 
 
-  console.log(`${LData.length}`);
+  //console.log(`${LData.length}`);
   arr.connect(cont.destination);
   arr.playbackRate.value = 0.5;
- // arr.start();
-  //console.log(buzova + " started");
+  arr.start();
+  console.log(buzova + " started");
 }
-//b.onclick=()=>{
+b.onclick=()=>{
 
   let cont=new AudioContext();
 
@@ -76,7 +83,7 @@ let BlockPos=0;
     });
 
 
-//};
+};
 
 
 
